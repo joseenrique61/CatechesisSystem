03e12a79f494bee77eff36b18925676e99710d59
José Enrique Cerezo <joseenrique61@hotmail.com>
2025-05-06 13:28:41 -0500

    Refactored some stuff

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, flash
 from app.auth.forms import ParishPriestForm
 from app.main.forms import ParishForm
-from app.main.models import Parish, Address, Location, User, ParishPriest, PhoneNumberType, Person, PhoneNumber, Role
-from app.main.model_utilities import insert_model
+from app.main.models import ParishPriest
+from app.main.model_utilities import *
 from app import db
 
 bp = Blueprint('admin', __name__)
@@ -12,94 +12,21 @@ bp = Blueprint('admin', __name__)
 def register_parish_priest():
     form = ParishPriestForm(request.form)
     if request.method == 'POST' and form.validate_on_submit():
-        user = User(
-            Username=form.User.Username.data,
-            Role=Role.query.filter_by(Role='ParishPriest').first(),
-        )
-        user.set_password(form.User.Password.data)
-        try:
-            user, correct = insert_model(user)
-            if not correct:
-                flash(f'El usuario {user.Username} ya existe.', 'danger')
-                return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-        except Exception as e:
-            print(f"Error inserting user: {e}")
-            flash('Error al registrar el usuario.', 'danger')
+        user, success = register_user(form.User)
+        if not success:
             return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
         
-        addressLocation = Location(
-            Country=form.Person.Address.Location.Country.data,
-            State=form.Person.Address.Location.State.data,
-            Province=form.Person.Address.Location.Province.data,
-        )
-        try:
-            addressLocation, correct = insert_model(addressLocation)
-        except Exception as e:
-            print(f"Error inserting location: {e}")
-            flash('Error al registrar la ubicación.', 'danger')
+        person, success = register_person(form.Person)
+        if not success:
             return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-        
-        address = Address(
-            MainStreet=form.Person.Address.MainStreet.data,
-            Number=form.Person.Address.Number.data,
-            SecondStreet=form.Person.Address.SecondStreet.data,
-            Location=addressLocation
-        )
-        try:
-            address, correct = insert_model(address)
-        except Exception as e:
-            print(f"Error inserting address: {e}")
-            flash('Error al registrar la dirección.', 'danger')
-            return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-
-        birthLocation = Location(
-            Country=form.Person.BirthLocation.Country.data,
-            State=form.Person.BirthLocation.State.data,
-            Province=form.Person.BirthLocation.Province.data,
-        )
-        try:
-            birthLocation, correct = insert_model(birthLocation)
-        except Exception as e:
-            print(f"Error inserting birth location: {e}")
-            flash('Error al registrar la ubicación de nacimiento.', 'danger')
-            return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-        
-        phoneNumber = PhoneNumber(
-            PhoneNumber=form.Person.PhoneNumber.data,
-            PhoneNumberType=PhoneNumberType.query.get(form.Person.PhoneNumberType.data),
-        )
-
-        person = Person(
-            FirstName=form.Person.FirstName.data,
-            MiddleName=form.Person.MiddleName.data,
-            FirstSurname=form.Person.FirstSurname.data,
-            SecondSurname=form.Person.SecondSurname.data,
-            DNI=form.Person.DNI.data,
-            BirthDate=form.Person.BirthDate.data,
-            BirthLocation=birthLocation,
-            Gender=form.Person.Gender.data,
-            PhoneNumber=phoneNumber,
-            Address=address,
-            EmailAddress=form.Person.Email.data,
-        )
-        try:
-            person, correct = insert_model(person)
-            if not correct:
-                flash(f'La persona {person.FirstName} {person.SecondName} {person.FirstSurname} {person.SecondSurname} ya existe.', 'danger')
-                db.session.rollback()
-                return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-        except Exception as e:
-            print(f"Error inserting person: {e}")
-            flash('Error al registrar la persona.', 'danger')
-            return render_template('admin/register_parish_priest.html', title='Registrar Sacerdote', form=form)
-        
+         
         parishPriest = ParishPriest(
             User=user,
             Person=person,
             IDParish=form.Parish.data,
         )
         try:
-            parishPriest, correct = insert_model(parishPriest)
+            parishPriest, correct, _ = insert_model(parishPriest, [['IDParish'], ['IDUser']])
             if not correct:
                 flash(f'El sacerdote {parishPriest.Person.FirstName} {parishPriest.Person.FirstSurname} ya existe.', 'danger')
                 return redirect(url_for('admin.register_parish_priest'))
--- app/main/model_utilities.py
+++ app/main/model_utilities.py
@@ -1,8 +1,10 @@
+from flask import flash
 from app import db
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy import inspect
 
-def insert_model(model) -> tuple[object, bool]:
+from app.main.models import Address, Location, Person, PhoneNumber, PhoneNumberType, Role, User
+
+def insert_model(model, attribute_lists: list[list[str]]) -> tuple[object, bool, int]:
     """
     Insert a model into the database.
     :param model: The model to insert.
@@ -11,41 +13,129 @@ def insert_model(model) -> tuple[object, bool]:
     try:
         db.session.add(model)
         db.session.flush()
-        return model, True
+        return model, True, None
     except IntegrityError as e:
         db.session.rollback()
-        # result = db.session.query(model.__class__).filter_by(**(model.__dict__)).one()
+        results = []
+        for attribute_list in attribute_lists:
+            filter_conditions = {}
 
-        mapper = inspect(type(model)) # Obtiene el mapper para la clase del modelo
-        filter_conditions = {}
+            for column_name in attribute_list:
+                if hasattr(model, column_name):
+                    value = getattr(model, column_name)
+                    filter_conditions[column_name] = value
+                else:
+                    print(f"El modelo no tiene el atributo {column_name}.")
 
-        for column in mapper.columns: # Itera sobre las columnas mapeadas
-            column_name = column.key # Nombre del atributo en el modelo
-            if hasattr(model, column_name):
-                value = getattr(model, column_name)
-                # Solo incluir en el filtro si el valor no es None
-                # y no es una clave primaria si el valor es el default (ej. 0 para int autoincremental no asignado)
-                # Esto último es más complejo de generalizar, por ahora solo verificamos None.
-                if value is not None:
-                    # Evitar claves primarias no establecidas si son autoincrementales
-                    # Esta lógica puede necesitar ser más robusta
-                    is_pk_and_default = False
-                    if column.primary_key:
-                        # Si es PK y el tipo es int y el valor es 0, podríamos asumirlo como no establecido
-                        # Esto es una simplificación.
-                        if isinstance(value, int) and value == 0:
-                                is_pk_and_default = True # Podría ser más específico (ej. si es autoincrement)
+            if not filter_conditions:
+                print("No se generaron condiciones de filtro válidas a partir de los atributos del modelo.")
+                return None, False
 
+            results.append(db.session.query(type(model)).filter_by(**filter_conditions).one_or_none())
 
-                    if not is_pk_and_default:
-                        filter_conditions[column_name] = value
-
-        if not filter_conditions:
-            print("No se generaron condiciones de filtro válidas a partir de los atributos del modelo.")
-            return None, False
-
-        print(f"Buscando {type(model).__name__} con filtros: {filter_conditions}")
-        return db.session.query(type(model)).filter_by(**filter_conditions).one_or_none(), False
+        result = next((result for result in results if result is not None), None)
+        return result, False, results.index(result)
     except Exception as e:
         db.session.rollback()
         raise e
+
+def register_user(userForm) -> tuple[User, bool]:
+    success = True
+    try:
+        user = User(
+            Username=userForm.Username.data,
+            Role=Role.query.filter_by(Role='ParishPriest').first(),
+        )
+        user.set_password(userForm.Password.data)
+
+        user, correct, _ = insert_model(user, [['Username']])
+        if not correct:
+            flash(f'El usuario {user.Username} ya existe.', 'danger')
+            success = False
+    except Exception as e:
+        print(f"Error inserting user: {e}")
+        flash('Error al registrar el usuario.', 'danger')
+        success = False
+    return user, success
+
+def register_location(locationForm) -> tuple[Location, bool]:
+    success = True
+    try:
+        location = Location(
+            Country=locationForm.Country.data,
+            State=locationForm.State.data,
+            Province=locationForm.Province.data,
+        )
+
+        location, _, _ = insert_model(location, [['Country', 'State', 'Province']])
+    except Exception as e:
+        print(f"Error inserting location: {e}")
+        flash('Error al registrar la ubicación.', 'danger')
+        success = False
+    return location, success
+
+def register_address(addressForm) -> tuple[Address, bool]:
+    success = True
+    try:
+        location, success = register_location(addressForm.Location)
+        address = Address(
+            MainStreet=addressForm.MainStreet.data,
+            Number=addressForm.Number.data,
+            SecondStreet=addressForm.SecondStreet.data,
+            Location=location,
+        )
+
+        address, _, _ = insert_model(address, [['MainStreet', 'Number', 'SecondStreet']])
+    except Exception as e:
+        print(f"Error inserting address: {e}")
+        flash('Error al registrar la dirección.', 'danger')
+        success = False
+    return address, success
+
+def register_phone(personForm) -> tuple[PhoneNumber, bool]:
+    success = True
+    try:
+        phoneNumber = PhoneNumber(
+            PhoneNumber=personForm.PhoneNumber.data,
+            PhoneNumberType=PhoneNumberType.query.get(personForm.PhoneNumberType.data),
+        )
+
+        phoneNumber, _, _ = insert_model(phoneNumber, [['PhoneNumber']])
+    except Exception as e:
+        print(f"Error inserting phone: {e}")
+        flash('Error al registrar el teléfono.', 'danger')
+        success = False
+    return phoneNumber, success
+
+def register_person(personForm) -> tuple[object, bool]:
+    success = True
+    try:
+        birthLocation, success = register_location(personForm.BirthLocation)
+        phoneNumber, success1 = register_phone(personForm)
+        address, success2 = register_address(personForm.Address)
+
+        person = Person(
+            FirstName=personForm.FirstName.data,
+            MiddleName=personForm.MiddleName.data,
+            FirstSurname=personForm.FirstSurname.data,
+            SecondSurname=personForm.SecondSurname.data,
+            DNI=personForm.DNI.data,
+            BirthDate=personForm.BirthDate.data,
+            BirthLocation=birthLocation,
+            Gender=personForm.Gender.data,
+            PhoneNumber=phoneNumber,
+            Address=address,
+            EmailAddress=personForm.Email.data,
+        )
+        person, correct, option = insert_model(person, [['FirstName', 'MiddleName', 'FirstSurname', 'SecondSurname'], ['DNI']])
+        if not correct:
+            if option == 0:
+                flash(f'La persona {person.FirstName} {person.MiddleName} {person.FirstSurname} {person.SecondSurname} ya existe.', 'danger')
+            elif option == 1:
+                flash(f'La persona con DNI {person.DNI} ya existe.', 'danger')
+            success = False
+    except Exception as e:
+        print(f"Error inserting person: {e}")
+        flash('Error al registrar la persona.', 'danger')
+        success = False
+    return person, success and success1 and success2
